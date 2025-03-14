a632ba4e7a61a5a3dc57d0fdfa6f1bcfcb845770
nizrum <nizrum215@yandex.ru>
2025-03-14 15:22:09 +0300

    feat: add CardView

---
+++ src/components/views/CardView.ts
@@ -0,0 +1,80 @@
+import { Component } from "../base/Component";
+import { ensureElement } from "../../utils/utils";
+
+export interface ICard<T> {
+	title: string;
+	description?: string | string[];
+	image: string;
+	status: T;
+}
+
+export class CardView<T> extends Component<ICard<T>> {
+	protected _title: HTMLElement;
+	protected _image?: HTMLImageElement;
+	protected _description?: HTMLElement;
+	protected _button?: HTMLButtonElement;
+
+	constructor(
+		protected blockName: string,
+		container: HTMLElement,
+		onClick?: (event: MouseEvent) => void
+	) {
+		super(container);
+
+		this._title = ensureElement<HTMLElement>(
+			`.${blockName}__title`,
+			container
+		);
+		this._image = ensureElement<HTMLImageElement>(
+			`.${blockName}__image`,
+			container
+		);
+		this._button = container.querySelector(`.${blockName}__button`);
+		this._description = container.querySelector(
+			`.${blockName}__description`
+		);
+
+		if (onClick) {
+			if (this._button) {
+				this._button.addEventListener("click", onClick);
+			} else {
+				container.addEventListener("click", onClick);
+			}
+		}
+	}
+
+	set id(value: string) {
+		this.container.dataset.id = value;
+	}
+
+	get id(): string {
+		return this.container.dataset.id || "";
+	}
+
+	set title(value: string) {
+		this.setText(this._title, value);
+	}
+
+	get title(): string {
+		return this._title.textContent || "";
+	}
+
+	set image(value: string) {
+		this.setImage(this._image, value, this.title);
+	}
+
+	set description(value: string | string[]) {
+		if (Array.isArray(value)) {
+			this._description.replaceWith(
+				...value.map((str) => {
+					const descTemplate =
+						this._description.cloneNode() as HTMLElement;
+					this.setText(descTemplate, str);
+					return descTemplate;
+				})
+			);
+		} else {
+			this.setText(this._description, value);
+		}
+	}
+}
